c4ad9de70c5206a3ad13bf5233ce7712a5e83e8f
Murat Özalp <cunyor@gmail.com>
2019-01-02 15:56:42 +0300

    birimleri de view'e gönderdim

--- listele/views.py
+++ listele/views.py
@@ -1,10 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import ip
+from .models import birim
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def ip_liste(request):
     ipler = ip.objects.all().order_by('eposta')
-    return render(request, 'ip_list.html', {'ipler': ipler})
+    birimler = birim.objects.all()
+    return render(request, 'ip_list.html', {'ipler': ipler,'birimler' : birimler})
